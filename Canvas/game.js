d058625d36113c6e17c6cdc662367b0f7a7d0867
AnthonyTT02 <129718569+AnthonyTT02@users.noreply.github.com>
2024-09-15 23:37:06 +0300

    8_osa первый взлет мяча под случайным углом

--- Canvas/game.js
+++ Canvas/game.js
@@ -5,12 +5,14 @@ const KEYS = {
 };
 
 let game = {
-    ctx:null,
+    ctx: null,
     platform: null,
     ball: null,
     blocks: [],
     rows: 4,
     cols: 8,
+    width: 640,
+    height: 360,
     sprites: {
         background: null,
         ball: null,
@@ -25,7 +27,7 @@ let game = {
         window.addEventListener("keydown", e => {
             if (e.keyCode === KEYS.SPACE) {
                 this.platform.fire();
-            } else if (e.keycode === KEYS.LEFT || e.keyCode === KEYS.RIGHT) {
+            } else if (e.keyCode === KEYS.LEFT || e.keyCode === KEYS.RIGHT) {
                 this.platform.start(e.keyCode);
             }
         });
@@ -73,6 +75,7 @@ let game = {
         });
     },
     render() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
         this.ctx.drawImage(this.sprites.background, 0, 0);
         this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
             this.ball.x, this.ball.y, this.ball.width, this.ball.height);
@@ -90,11 +93,15 @@ let game = {
             this.create();
             this.run();
         });
+    },
+    random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
 };
 
 game.ball = {
+    dy: 0,
     dy: 0,
     velocity: 3,    
     x: 320,
@@ -103,10 +110,14 @@ game.ball = {
     height: 20,
     start() {
         this.dy = -this.velocity;
+        this.dx = game.random(-this.velocity, this.velocity);
     },
     move() {
         if (this.dy) {
             this.y += this.dy;
+        }
+        if (this.dx) {
+            this.x += this.dx;
         }
     }
 };
