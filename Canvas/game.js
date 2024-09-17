dcaf3edf771511f160ac6f4a3866ed5f01bc8d6e
AnthonyTT02 <129718569+AnthonyTT02@users.noreply.github.com>
2024-09-17 10:22:14 +0300

    13_osa не даем платформе выйти за края экрана

--- Canvas/game.js
+++ Canvas/game.js
@@ -68,6 +68,7 @@ let game = {
         this.collideBlocks();
         this.collidePlatform();
         this.ball.collideWorldBounds();
+        this.platform.collideWorldBounds();
         this.platform.move();
         this.ball.move();
     },
@@ -182,9 +183,15 @@ game.ball = {
         block.active = false;
     },
     bumpPlatform(platform) {
-        this.dy *= -1;
-        let touchX = this.x + this.width / 2;
-        this.dx = this.velocity * platform.getTouchOffset(touchX);
+        if (platform.dx) {
+            this.x += platform.dx;
+        }
+
+        if (this.dy > 0) {
+            this.dy = -this.velocity;
+            let touchX = this.x + this.width / 2;
+            this.dx = this.velocity * platform.getTouchOffset(touchX);
+        }
     }
 
 };
@@ -226,8 +233,19 @@ game.platform = {
         let offset = this.width - diff;
         let result = 2 * offset / this.width;
         return result - 1;
+    },
+    collideWorldBounds() {
+        let x = this.x + this.dx;
+        let platformLeft = x;
+        let platformRight = platformLeft + this.width;
+        let worldLeft = 0;
+        let worldRight = game.width;
+
+        if (platformLeft < worldLeft || platformRight > worldRight) {
+            this.dx = 0;
+        }
     }
-};
+ };
 
 window.addEventListener("load", () => {
     game.start();
