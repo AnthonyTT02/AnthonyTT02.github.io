1f55d7fe1ab3f4c984366c62c0b0a07076205320
AnthonyTT02 <129718569+AnthonyTT02@users.noreply.github.com>
2024-10-25 13:15:50 +0300

    project_iceandfire

---
+++ react_2/precache-manifest.0e51c191cfe5ec4ce91febb613a34793.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d9421e453ec10ffa28f",
+    "url": "/react_2/static/css/main.981311d7.chunk.css"
+  },
+  {
+    "revision": "0d9421e453ec10ffa28f",
+    "url": "/react_2/static/js/main.0d9421e4.chunk.js"
+  },
+  {
+    "revision": "cca92055314f3acd70ca",
+    "url": "/react_2/static/css/1.6a4f9cce.chunk.css"
+  },
+  {
+    "revision": "cca92055314f3acd70ca",
+    "url": "/react_2/static/js/1.cca92055.chunk.js"
+  },
+  {
+    "revision": "99de156d63cdeb226983",
+    "url": "/react_2/static/js/runtime~main.99de156d.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/react_2/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/react_2/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "9d29553b899a8ddd9fee8f640b4c5e01",
+    "url": "/react_2/index.html"
+  }
+];
