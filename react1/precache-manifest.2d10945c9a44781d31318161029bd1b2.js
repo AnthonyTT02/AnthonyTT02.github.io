f044f825aa97934cf2248e2fab25a7afe5711515
AnthonyTT02 <129718569+AnthonyTT02@users.noreply.github.com>
2024-10-29 09:15:40 +0200

    done

---
+++ react1/precache-manifest.2d10945c9a44781d31318161029bd1b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8286831cae4ea24c08da",
+    "url": "/react1/static/css/main.eb55f06f.chunk.css"
+  },
+  {
+    "revision": "8286831cae4ea24c08da",
+    "url": "/react1/static/js/main.8286831c.chunk.js"
+  },
+  {
+    "revision": "bd3103f1382550607871",
+    "url": "/react1/static/css/1.0720936e.chunk.css"
+  },
+  {
+    "revision": "bd3103f1382550607871",
+    "url": "/react1/static/js/1.bd3103f1.chunk.js"
+  },
+  {
+    "revision": "a74937258285e9ed0a2e",
+    "url": "/react1/static/js/runtime~main.a7493725.js"
+  },
+  {
+    "revision": "3f530093df70b33d080bcd4b15bbdd05",
+    "url": "/react1/index.html"
+  }
+];
---
+++ react1/precache-manifest.b1e8158cdc699b1213b25bc8294139dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8286831cae4ea24c08da",
+    "url": "/react_1/static/css/main.eb55f06f.chunk.css"
+  },
+  {
+    "revision": "8286831cae4ea24c08da",
+    "url": "/react_1/static/js/main.8286831c.chunk.js"
+  },
+  {
+    "revision": "bd3103f1382550607871",
+    "url": "/react_1/static/css/1.0720936e.chunk.css"
+  },
+  {
+    "revision": "bd3103f1382550607871",
+    "url": "/react_1/static/js/1.bd3103f1.chunk.js"
+  },
+  {
+    "revision": "ba0dfd9aa5e90884e86f",
+    "url": "/react_1/static/js/runtime~main.ba0dfd9a.js"
+  },
+  {
+    "revision": "7048d098c4aec3246448e4865dda1631",
+    "url": "/react_1/index.html"
+  }
+];
